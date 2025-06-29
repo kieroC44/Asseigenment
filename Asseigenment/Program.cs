ec991202b6ef98baaf79b1e20495180b580d98f4
kieroc44 <kierolleskamel@gmail.com>
2025-06-30 00:30:52 +0300

    Add project files.

---
+++ Asseigenment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Asseigenment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
