0385b9acabdd2be9e1fc4ea30e396f5c508c5614
kieroc44 <kierolleskamel@gmail.com>
2025-06-30 22:15:25 +0300

    Q19

--- Asseigenment/Program.cs
+++ Asseigenment/Program.cs
@@ -316,21 +316,24 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
             #endregion
 
             #region Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.
+            //Console.WriteLine("Enter the size of the identity matrix (n):");
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        if (i == j)
+            //            Console.Write("1 ");
+            //        else
+            //            Console.Write("0 ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
 
             #endregion
 
