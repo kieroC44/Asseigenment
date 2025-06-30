cfdc248c2fe98aaf80d0b619bfd0843feb9ce327
kieroc44 <kierolleskamel@gmail.com>
2025-06-30 22:17:31 +0300

    Q20

--- Asseigenment/Program.cs
+++ Asseigenment/Program.cs
@@ -338,6 +338,20 @@
             #endregion
 
             #region Write a program in C# Sharp to find the sum of all elements of the array.
+            //Console.Write("Enter number of elements: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int[] arr = new int[n];
+            //int sum = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"Enter element {i + 1}: ");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //    sum += arr[i];
+            //}
+
+            //Console.WriteLine("Sum = " + sum);
 
             #endregion
 
